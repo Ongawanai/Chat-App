b0d37a0b5c054837b93066e65f21782b17cce157
Ongawanai <ssaai@yandex.ru>
2023-07-19 19:45:24 +0500

    fixing linter issues

--- frontend/src/hooks/index.js
+++ frontend/src/hooks/index.js
@@ -1,7 +1,6 @@
 import { useContext } from 'react';
 
-import authContext from '../contexts/index.js';
+import authContext from '../contexts/authContext';
 
-const useAuth = () => useContext(authContext);
+export const useAuth = () => useContext(authContext);
 
-export default useAuth;
